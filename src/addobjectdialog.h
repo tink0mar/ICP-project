6109031378acc2e90df713402569b09aedd2a1df
unknown <plocica.peter@gmail.com>
2022-05-09 12:43:05 +0200

    Pridanie komentarov k sekvencnemu diagramu

--- src/addobjectdialog.h
+++ src/addobjectdialog.h
@@ -7,25 +7,68 @@ namespace Ui {
 class AddObjectDialog;
 }
 
+/**
+ * @brief AddObjectDialog class used to create a window for creation of object.
+ */
 class AddObjectDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor.
+     * @param parent Parent window.
+     */
     explicit AddObjectDialog(QWidget *parent = nullptr);
+
+    /**
+     * @brief Deconstructor.
+     */
     ~AddObjectDialog();
 
+    /**
+     * @brief Fills combobox with available classes.
+     * @param classes Available classes.
+     */
     void SetClasses(QStringList classes);
+
+    /**
+     * @brief Returns name of the object.
+     * @return String representation of the name.
+     */
     QString GetName();
+
+    /**
+     * @brief Returns type (selected class) of the object.
+     * @return String representation of type.
+     */
     QString GetType();
+
 private slots:
+    /**
+     * @brief Confirmation of the selection.
+     */
     void on_btnOK_clicked();
 
+    /**
+     * @brief Cancelation.
+     */
     void on_btnCancel_clicked();
 
 private:
+    /**
+     * @brief User interface.
+     */
     Ui::AddObjectDialog *ui;
+
+    /**
+     * @brief Name of the object.
+     */
     QString name;
+
+    /**
+     * @brief type Type of the object.
+     */
     QString type;
 };
 
--- src/editmessagedialog.h
+++ src/editmessagedialog.h
@@ -7,26 +7,75 @@ namespace Ui {
 class EditMessageDialog;
 }
 
+/**
+ * @brief EditMessageDialog class used to create a window for editing a message/event.
+ */
 class EditMessageDialog : public QDialog
 {
     Q_OBJECT
 
 public:
+    /**
+     * @brief Constructor.
+     * @param parent Parent window.
+     */
     explicit EditMessageDialog(QWidget *parent = nullptr);
+
+    /**
+     * @brief Deconstructor.
+     */
     ~EditMessageDialog();
 
+    /**
+     * @brief Sets object into read-only line edit box.
+     * @param o Object.
+     */
     void SetObj1(QString o);
+
+    /**
+     * @brief Sets object into read-only line edit box.
+     * @param o Object.
+     */
     void SetObj2(QString o);
+
+    /**
+     * @brief Sets type into read-only line edit box.
+     * @param type Type of the message.
+     */
     void SetType(QString type);
+
+    /**
+     * @brief Sets description into line edit box.
+     * @param Message description.
+     */
     void SetMsg(QString msg);
+
+    /**
+     * @brief Returns new message description.
+     * @return String representation of message description.
+     */
     QString GetMsg();
+
 private slots:
+    /**
+     * @brief Confirmation of changes.
+     */
     void on_btnOK_clicked();
 
+    /**
+     * @brief Cancelation.
+     */
     void on_btnCancel_clicked();
 
 private:
+    /**
+     * @brief ui User interface.
+     */
     Ui::EditMessageDialog *ui;
+
+    /**
+     * @brief New message description.
+     */
     QString msg;
 };
 
